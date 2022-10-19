d7e2630e9c2802af5fdf9405c1e9f13911f2ca4d
shoheiyonekura <shohei.yonekura.t5@dc.tohoku.ac.jp>
2022-10-19 12:52:42 +0900

    css修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Button from '../components/button'
 
 const Home: NextPage = () => {
   return (
   <div className="container mx-auto">
-    <h1 className="text-center">トマト</h1>
-    <div className="text-center">
-      <Link href="/input">
-          <a>入力画面へ</a>
-      </Link>
-    </div>
     <div className="planter text-center mx-auto">
-      <a>タスクなし</a>
+      <h3>タスクはありません</h3>
       <div>
         <Image src="/images/nae.png" width={200} height={150}/>
       </div>
+    </div>
+    <div className="text-center">
+      <Link href="/input">
+          <Button>タスクを入力する</Button>
+      </Link>
     </div>
   </div>
   )
--- pages/input.tsx
+++ pages/input.tsx
@@ -1,15 +1,20 @@
+import React from "react"
 import Head from 'next/head'
 import Link from 'next/link' //追記
+import Button from '../components/button'
+import { useForm,SubmitHandler } from "react-hook-form"
+
 
 const InputPage = () => {
   return (
     <div className="container text-center mx-auto">
-      <h1 className="text-4xl text-green-700">タスク入力</h1>
+      <h1 className="text-4xl text-green-700 font-logo">タスク入力</h1>
         <div className="item-center">
             <h3 className="">タスクの内容</h3>
+            <textarea className="p-2.5 w-full bg-gray-50 rounded-lg border border-gray-300"></textarea>
             <h3 className="">タスク優先度</h3>
             <div  className="">
-                <select>
+                <select className="rounded-lg">
                     <option>---数字が小さい方が優先度高い---</option>
                     <option>1</option>
                     <option>2</option>
@@ -20,11 +25,12 @@ const InputPage = () => {
             </div>
             <h3 className="">期限</h3>
             <div  className="">
-                <input type="date"></input>
+                <input type="date" className="rounded-lg"></input>
             </div>
             <h3 className="">タグ</h3>
+            <textarea className="p-2.5 w-full bg-gray-50 rounded-lg border border-gray-300"></textarea>
         </div>
-        
+        <Button>追加</Button>
     </div>
   )
 }
