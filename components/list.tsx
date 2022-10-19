e9216793d49c203e0d2f6cd20ec458718511548a
shoheiyonekura <shohei.yonekura.t5@dc.tohoku.ac.jp>
2022-10-19 18:17:51 +0900

    優先度*定数で画像を表示

--- components/list.tsx
+++ components/list.tsx
@@ -1,5 +1,6 @@
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { Todo, todoListState } from "../atoms/states";
+import Image from 'next/image'
 
 const TodoList = () => {
     const [todoList, setTodoList] = useRecoilState(todoListState)
@@ -10,13 +11,19 @@ const TodoList = () => {
         })
         setTodoList(target);
     }
-
     return (
-        <div>
+        <div className="text-center mx-auto w-2/4">
             {todoList.map(item =>
-                <div key={item.id}>
-                    タイトル:{item.title} <br />
-                    テキスト:{item.priority} <br />
+                <div key={item.id} className="border-black p-2.5 my-2 border-solid border-2 ">
+                    <div className=" bg-gray-500">
+                    {item.title}
+                    </div>
+                    優先度:{item.priority} <br />
+
+                    日付:{item.date} <br />
+                    <div className="">
+                    <Image src="/images/tomatoRed.png" width={item.priority * 25} height={item.priority * 25} />
+                    </div>
                     <button  onClick={() => { deleteTodo(item.id) }} >
                         完了
                     </button>
