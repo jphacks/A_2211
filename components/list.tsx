7d7bda346a642028675e85ea737fa0bf507b5417
shoheiyonekura <shohei.yonekura.t5@dc.tohoku.ac.jp>
2022-10-20 20:50:37 +0900

    トマト枠の外に配置

--- components/list.tsx
+++ components/list.tsx
@@ -1,6 +1,8 @@
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { Todo, todoListState } from "../atoms/states";
 import Image from 'next/image'
+import dayjs from 'dayjs'
+import Nae from '../components/nae'
 
 const TodoList = () => {
     const [todoList, setTodoList] = useRecoilState(todoListState)
@@ -11,22 +13,34 @@ const TodoList = () => {
         })
         setTodoList(target);
     }
+    
+    todoList.map(item =>{
+        const dateTask = dayjs(item.date);
+        const now = dayjs();
+        const day = now.diff(dateTask, 'day', false);
+        const hoge = (day: number) => {
+            if(day <= 3)  return <Image src="/images/tomatoRed.png" width={item.priority * 25} height={item.priority * 25} />;
+            else return <Nae />
+            }
+    })
     return (
         <div className="text-center mx-auto w-2/4">
             {todoList.map(item =>
-                <div key={item.id} className="border-black p-2.5 my-2 border-solid border-2 ">
-                    <div className=" bg-gray-500">
-                    {item.title}
-                    </div>
-                    優先度:{item.priority} <br />
-
-                    日付:{item.date} <br />
-                    <div className="">
+                <div key={item.id} className="sm:grid sm:grid-cols-2">
+                    <div className="mx-auto my-auto">
                     <Image src="/images/tomatoRed.png" width={item.priority * 25} height={item.priority * 25} />
                     </div>
-                    <button  onClick={() => { deleteTodo(item.id) }} >
-                        完了
-                    </button>
+                    <div className="border-black p-2.5 my-2 border-solid border-2 rounded-lg ">
+                        <div className=" bg-gray-500">
+                        {item.title}
+                        </div>
+                        優先度:{item.priority} <br />
+                        日付:{item.date} <br />
+
+                        <button  onClick={() => { deleteTodo(item.id) }} >
+                            完了
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
